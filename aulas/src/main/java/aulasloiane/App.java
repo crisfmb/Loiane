ef0ac894a978d1133c6d32eb2d68863192e82ebc
Cristina Borges <cristinaborges@cristinas-mbp.home>
2020-07-13 21:29:11 +0100

    primeiro

---
+++ aulas/src/main/java/aulasloiane/App.java
@@ -0,0 +1,13 @@
+package aulasloiane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
